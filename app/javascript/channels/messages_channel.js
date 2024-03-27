5ba62a446d9b4b4e776b5cd345dc2f943b298e9b
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2024-03-27 18:33:29 -0400

    set up connection a bit different

--- app/javascript/channels/messages_channel.js
+++ app/javascript/channels/messages_channel.js
@@ -3,10 +3,12 @@ import consumer from "./consumer"
 consumer.subscriptions.create("MessagesChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log('connected')
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    console.log('disconnected')
   },
 
   received(data) {
