7849e9984e4965b74fced89ea78b8d474825e16c
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2025-03-10 16:28:39 -0400

    small config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "localhost"
+    "localhost",
+    "vincula.nasean.dev"
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
